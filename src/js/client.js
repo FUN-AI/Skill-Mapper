8166c3299b5e2ecd1ad5eb98267acba77f48510e
EbichanOO <takanori.sao@icloud.com>
2018-12-13 19:53:49 +0900

    now writting

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactBootstrap from "react-bootstrap";
+
+var Grid = ReactBootstrap.Grid;
+var Row = ReactBootstrap.Row;
+var Col = ReactBootstrap.Col;
+var Button = ReactBootstrap.Button;
 
 class Layout extends React.Component {
+  getTopicTags(){
+    return ["AI", "C#", "C++", "javascript"];
+  }
+  showtagButton(tags){
+    for()
+    <Button>
+
+    </Button>
+  }
+
+  firstLayer(){
+    <div>
+      <Grid>
+        
+      </Grid>
+    </div>
+  }
   render() {
     return (
-      <h1>Welcome!</h1>
+      <div>
+        <Grid>
+
+        </Grid>
+      </div>
     );
   }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Layout/>, document.getElementById('app'));
